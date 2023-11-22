509946e2d4b56203ff75f8fa1ce9a49f24a97be4
Sayantika Pal <pal.sayan@northeaster.edu>
2023-11-22 16:48:25 -0500

    Committing grant to inventory_manager and adding product_manager queries

--- 3_Grant_App_admin.sql
+++ 3_Grant_App_admin.sql
@@ -31,3 +31,22 @@ grant select on vw_order_table to product_manager;
 --revoke select on user_product from product_manager;
 --revoke select on payment from product_manager;
 --revoke select on order_table from product_manager;
+
+--select * from PRODUCT;
+----
+----
+----
+--inventory_manager
+create user inventory_manager identified by PasswordMaverick3;
+grant create session to inventory_manager;
+grant execute on add_warehouse to inventory_manager;
+grant execute on add_ware_product to inventory_manager;
+grant select on vw_product to inventory_manager;
+grant select on vw_product_category to inventory_manager;
+grant select on vw_ware_product to inventory_manager;
+grant select on vw_warehouse to inventory_manager;
+
+--revoke select on product from inventory_manager;
+--revoke select on product_category from inventory_manager;
+--revoke select on ware_product from inventory_manager;
+--revoke select on warehouse from inventory_manager;
